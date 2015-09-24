4ecc3cc2e4c8ab1236306b7eb304eb9829362c3a
thoppe <travis.hoppe@gmail.com>
2015-09-24 00:04:12 -0400

    Infer module now takes in get_parameters

--- peacock/infer.py
+++ peacock/infer.py
@@ -2,9 +2,8 @@ from swagger_spec import *
 from urlparse import urlsplit, parse_qs
 import datetime, ast
 import dateutil.parser
-#yourdate = dateutil.parser.parse(datestring)
-#print type(ast.literal_eval('True'))
-#exit()
+import logging
+logging.basicConfig(level=logging.INFO)
 
 def infer_type(x):
     # First try a common type
@@ -24,6 +23,58 @@ def infer_type(x):
     # Got nothing? Return a string
     return str
 
+def get_parameters(url):
+    '''
+    Returns a list of Swagger parameters from an input url. Types are infered.
+    '''
+        
+    swagger_type_lookup = {
+        str : "string",
+        unicode : "string",
+        float   : "number",
+        int     : "integer",
+        bool    : "boolean",
+        datetime.datetime : "string",
+    }
+
+    # Split the query string into key/value pairs
+    p = urlsplit(url)
+    query = parse_qs(p.query,keep_blank_values=True)
+
+    parameters = []
+
+    for key,val in query.items():
+        
+        # Ignore all but first query parameter (e.g. if it was called twice)
+        val = val.pop()
+
+        # Determine the python type of the object
+        obj_type = infer_type(val)
+
+        # Determine the swagger type of the object (this is restricted)
+        swagger_type = swagger_type_lookup[obj_type]
+
+        # Set some example text
+        text = "Example value: {}".format(val)
+
+        
+        p_args = {
+            "name"   :key,
+            "type000":swagger_type,
+            "description":text
+        }
+
+        # If the object is a datetime we can set the format
+        if obj_type == datetime.datetime:
+            p_args["format"] = "date-time"
+
+        # Add the parameter
+        parameters.append(Parameter(**p_args))
+
+    return parameters
+    
+    
+
 with open("../api_key_demo.md") as FIN:
     API_KEY = FIN.read().strip()    
 
@@ -37,15 +88,9 @@ S.host = p.netloc
 S.basePath = "/"
 
 S.paths[p.path] = path = Path()
-x = path.get
+path.get.responses = Responses({"200":Response()})
 
-query = parse_qs(p.query,keep_blank_values=True)
-oper = Operation()
-oper.responses = Responses({"200":Response()})
-params = oper.parameters
-for key,val in query.items():
-    
-    # Ignore all but first query parameter
-    val = val.pop()
-    print key,val,infer_type(val)
+parameters = Parameters()
+path.get.parameters = get_parameters(url)
+
 print S
