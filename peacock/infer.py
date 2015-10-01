4a2ccbc9a9c21d0239c5c76a91d9b0265af7397b
thoppe <travis.hoppe@gmail.com>
2015-10-01 00:41:40 -0400

    Working on infer from GET response

--- peacock/infer.py
+++ peacock/infer.py
@@ -6,8 +6,8 @@ import logging
 import requests
 
 import bs4
-#import requests_cache
-#requests_cache.install_cache('debug_cache')
+import requests_cache
+requests_cache.install_cache('debug_cache')
 
 logging.basicConfig(level=logging.INFO)
 
@@ -22,33 +22,57 @@ def infer_type(x):
     try:
         obj = ast.literal_eval(x)
         return type(obj)
-    except ValueError:
+    except (ValueError,SyntaxError):
         pass
     
     # Now try a datetime
     try:
         obj = dateutil.parser.parse(x)
         return type(obj)
-    except ValueError:
+    except (ValueError,AttributeError):
         pass
 
     # Got nothing? Return a string
     return str
 
+swagger_type_lookup = {
+    str : "string",
+    unicode : "string",
+    float   : "number",
+    int     : "integer",
+    bool    : "boolean",
+    datetime.datetime : "string",
+}
+
+def infer_swagger_type(key,x,infer_base_type=True):
+           
+    # Possibly determine the python type of the object
+    if infer_base_type:
+        obj_type = infer_type(x)
+    else:
+        obj_type = type(x)
+
+    # Determine the swagger type of the object (this is restricted)
+    swagger_type = swagger_type_lookup[obj_type]
+
+    # Set some example text
+    text = "Example: {}".format(x)
+
+    p_args = {"type000":swagger_type}
+
+    # If the object is a datetime we can set the format
+    if obj_type == datetime.datetime:
+        p_args["format"] = "date-time"
+
+    p_args["name"] = key
+    p_args["description"] = text
+    
+    return p_args
+
 def get_parameters(url):
     '''
     Returns a list of Swagger parameters from an input url. Types are infered.
     '''
-        
-    swagger_type_lookup = {
-        str : "string",
-        unicode : "string",
-        float   : "number",
-        int     : "integer",
-        bool    : "boolean",
-        datetime.datetime : "string",
-    }
-
     # Split the query string into key/value pairs
     p = urlsplit(url)
     query = parse_qs(p.query,keep_blank_values=True)
@@ -60,26 +84,8 @@ def get_parameters(url):
         # Ignore all but first query parameter (e.g. if it was called twice)
         val = val.pop()
 
-        # Determine the python type of the object
-        obj_type = infer_type(val)
-
-        # Determine the swagger type of the object (this is restricted)
-        swagger_type = swagger_type_lookup[obj_type]
-
-        # Set some example text
-        text = "Example value: {}".format(val)
-
+        p_args = infer_swagger_type(key, val)
         
-        p_args = {
-            "name"   :key,
-            "type000":swagger_type,
-            "description":text
-        }
-
-        # If the object is a datetime we can set the format
-        if obj_type == datetime.datetime:
-            p_args["format"] = "date-time"
-
         # Add the parameter
         parameters.append(Parameter(**p_args))
 
@@ -97,13 +103,31 @@ def build_core_swagger_from_url(url):
 
 def build_get_path(url):
     path = Path()
-    path.get.responses  = Responses({"200":Response()})
+    path.get.responses  = res = Responses({"200":Response()})
     path.get.parameters = get_parameters(url)
 
     # request the url
     r = requests.get(url)
-    print r
+
     if r.status_code == 200:
+
+        data = r.json()
+        data.pop("url")
+
+                    
+        prop = res["200"].schema.properties
+
+        parameters = []
+
+        for key,val in r.json().items():
+            p_args = infer_swagger_type(key, val, False)
+            print Parameter(type000="object")
+            exit()
+            parameters.append(Parameter(**p_args))
+                        
+            #prop[key] = p_args
+            print key, val, p_args
+        print parameters
         print "ROCK AND ROLL!"
     else:
         soup = bs4.BeautifulSoup(r.content, 'html.parser')
